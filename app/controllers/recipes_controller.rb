50745cb875ed440ae0c1d006d87f686447e70a0f
Veronica Bustabad <vbustabad@gmail.com>
2018-08-12 22:25:09 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(ingredients_attributes: 'first ingredient')
-    @recipe.ingredients.build(ingredients_attributes: 'second ingredient')
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
